30a16fd50839b5750020b28d8305bd50fa3f1bc7
Hazem Zaki <hazemzaki.work@gmail.com>
2023-09-29 20:37:16 +0300

    Add Lec 3: C Basics Homework 1 Exercises Code

---
+++ Assignments/Lec3_C_Basics/src/main.c
@@ -0,0 +1,124 @@
+/*
+ ============================================================================
+ Name        : main.c
+ Author      : Hazem Zaki
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+	/* The following are Homework-1 Exercises:  */
+
+		/* Ex.1 */
+	/*
+	{
+		printf("C Programming");
+	}
+	*/
+
+		/* Ex.2 */
+	/*
+	{
+		int userInput;
+
+		printf("Enter an integer: ");
+		fflush(stdout);
+		scanf("%d", &userInput);
+
+		printf("You entered: %d", userInput);
+	}
+	*/
+
+		/* Ex.3 */
+	/*
+	{
+		int num1, num2;
+
+		printf("Enter two integers: ");
+		fflush(stdout);
+		scanf("%d", &num1);
+		scanf("%d", &num2);
+
+		printf("Sum: %d", num1 + num2);
+	}
+	*/
+
+		/* Ex.4 */
+	/*
+	{
+		float num1, num2;
+
+		printf("Enter two numbers: ");
+		fflush(stdout);
+		scanf("%f", &num1);
+		scanf("%f", &num2);
+
+		printf("Product: %f", num1 * num2);
+	}
+	*/
+
+		/* Ex.5 */
+	/*
+	{
+		char userInput;
+
+		printf("Enter a character: ");
+		fflush(stdout);
+		scanf("%c", &userInput);
+
+		printf("ASCII value of %c = %d", userInput, userInput);
+	}
+	*/
+
+		/* Ex.6 */
+	/*
+	{
+		float a, b;
+
+		printf("Enter value of a: ");
+		fflush(stdout);
+		scanf("%f", &a);
+		printf("Enter value of b: ");
+		fflush(stdout);
+		scanf("%f", &b);
+
+		float temp = a;
+		a = b;
+		b = temp;
+
+		printf("\nAfter swapping, value of a = %f", a);
+		printf("\nAfter swapping, value of b = %f", b);
+	}
+	*/
+
+		/* Ex.7 */
+	/*
+	{
+		int a, b;
+
+		printf("Enter value of a: ");
+		fflush(stdout);
+		scanf("%d", &a);
+		printf("Enter value of b: ");
+		fflush(stdout);
+		scanf("%d", &b);
+
+		a = a * b;
+		b = a / b;
+		a = a / b;
+
+		printf("\nAfter swapping, value of a = %d", a);
+		printf("\nAfter swapping, value of b = %d", b);
+	}
+	*/
+
+
+	/*========================================================================*/
+
+
+
+
+	return 0;
+}
